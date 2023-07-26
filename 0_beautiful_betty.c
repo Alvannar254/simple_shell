ad7799a07bf2614f110a812575a69703f3d0b07c
Älving Nganga <lanton254k@gmail.com>
2023-07-26 21:37:02 +0300

    Shell

--- 0_beautiful_betty.c
+++ 0_beautiful_betty.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
+ * This function uses the printf function to print the string "Hello, World"
+ * followed by a newline to the standard output.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	/* Print the string "Hello, World" followed by a newline */
 	printf("Hello, World\n");
+
+	/* Return 0 to indicate successful execution */
 	return (0);
 }
 
