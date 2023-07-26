e3ac7cc97c1ab8956599beab13df320200e9d6b7
Älving Nganga <lanton254k@gmail.com>
2023-07-26 07:30:45 +0300

    task 0 simple shell

---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Empty main.h file for Task 0 */
+
+#endif /* MAIN_H */
+
---
+++ task0_passbetty.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("This is the shell Project!\n");
+	return (0);
+}
+
