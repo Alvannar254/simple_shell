0277d2ccb5b173ea465ba32a0b39fac101e7f1ea
Älving Nganga <lanton254k@gmail.com>
2023-07-26 19:58:23 +0300

    simple shell

--- task0.c
+++ task0.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point of the program
  *
- * Description: This program prints "This is the shell Project!"
- *
  * Return: Always 0
  */
 int main(void)
 {
+	/* Print the message "This is the shell Project!" */
 	printf("This is the shell Project!\n");
+
 	return (0);
 }
 
