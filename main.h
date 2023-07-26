0e361d7b18bf47a7d9c64fbd102857d06d864836
Älving Nganga <lanton254k@gmail.com>
2023-07-26 21:28:12 +0300

    shell

--- main.h
+++ main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include "shell.h"
+extern char **environ;
+
+char *find_path(char *cmd);
+int execute_command(char *line);
+char *_getenv(const char *name);
+void handle_exit(char **args);
+void handle_env(char **args);
 
 #endif /* MAIN_H */
 
