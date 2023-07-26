b29a6df997538afb95e1c6fa0296134cbcab6f10
Andenga <Andengalydia@gmail.com>
2023-07-27 00:26:39 +0300

    Betty checks

---
+++ task_0_passbetty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "This is the shell Project!"
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("This is the shell Project!\n");
+	return (0);
+}
+
+
